173c8cb9fa382415193aa26be746e57c1c48492f
Dilshat Hewzulla <hewzulla@gmail.com>
2020-03-11 15:35:23 +0000

    updated expiration of cache on copy

--- src/app/DisplayCachedForm.js
+++ src/app/DisplayCachedForm.js
@@ -2,7 +2,9 @@ import React, {useState, useEffect } from "react";
 
 
 import * as formUtil from './formUtil';
+import * as chromeExtensionUtil from './chromeExtensionUtil'
 import * as cacheFields from './cacheFields';
+
 import {DisplayInputCopyField,TextButton,FormContainer} from './app-layout';
 
 export default ({gotoMobileIntegration,cachedFieldValues, domain})=>{          
@@ -12,12 +14,26 @@ export default ({gotoMobileIntegration,cachedFieldValues, domain})=>{
          cacheFields.clearFields();               
       }
    },[]);
+   const onCopied=()=>{
+      console.log("*************");
+      
+     if(cachedFieldValues.length<2){
+          return;
+     }
+     const numberOfNotEmptyFields=cachedFieldValues.reduce((count,f)=>f.value?count+1:count,0);
+     if(numberOfNotEmptyFields<0){
+          return;
+     }
+     const key=cacheFields.cacheFields(cachedFieldValues);
+     chromeExtensionUtil.sendKey(key);
+   }
 
           return (
                <FormContainer title="Cached Values" domain={domain}>              
                   {cachedFieldValues.map((formField,index)=>(<DisplayInputCopyField 
                   field={formField} 
                   hideValue={true}
+                  onCopied={onCopied}
                   key={formField.id}/>))}                                    
                   <TextButton onClick={gotoMobileIntegration} label={'Back'}/>   
 
